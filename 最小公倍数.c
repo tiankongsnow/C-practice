035251e527f270ff8829da0dfc1a823c3f233c1e
YUWENHUI <41729370+yoyoyohui@users.noreply.github.com>
2019-10-17 00:03:02 +0800

    最小公倍数和最大公因数

---
+++ 最小公倍数.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    long long a, b;
+    long long c, m, n;
+    while(scanf("%lld" "%lld", &a, &b)!=EOF){
+        if(a==0 && b==0){
+            break;
+        }
+        m=a;
+        n=b;
+        while(b!=0){  /* 余数不为0，继续相除，直到余数为0 */ 
+            c=a%b; 
+            a=b;  //最大公因数   最小公倍数=两整数的乘积÷最大公约数
+            b=c;
+        }
+        printf("最大公约数:%lld\n", a);
+        printf("最小公倍数:%lld\n", m*n/a);
+    }
+}
