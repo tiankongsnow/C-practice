4b3045000621c76313312344f0505f8433f4e3dd
YUWENHUI <41729370+yoyoyohui@users.noreply.github.com>
2019-11-20 23:45:50 +0800

    迭代和递归算费氏数列第n项 0 1 1 2 3 5 8 13 21........
    迭代和递归算费氏数列第n项 0 1 1 2 3 5 8 13 21........

--- 费氏数列.c
+++ 费氏数列.c
@@ -1,17 +1,18 @@
+/*0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233...*/
 #include <stdio.h>
-/*迭代*/
+/*Iterative Algorithm*/
 long long Fbnl(long N){
     long a1 = 0;
     long a2 = 1;
     long a3 = N;
     for (int i = 1; i <N; i++){
         a3 = a2 + a1;
-        a1= a2;
+        a1 = a2;
         a2 = a3;
     }
     return a3;
 }
-/*递归*/
+/*Recursive Algorithm*/
 long long Fbn2(long n){
      if (n <= 0){
          return 0 ;
@@ -25,14 +26,15 @@ long long Fbn2(long n){
 }
 int main(){
     int n;
+    printf("please input the pos(0~91):");
     while(scanf("%d", &n) != EOF){
         if(n>92){
             break;
         }
         long tmp = Fbnl(n);
         long tmp1 = Fbn2(n);
-        printf("迭代算法 %ld\n", tmp);
-        printf("递归算法 %ld", tmp1);
+        printf("Iterative algorithm/ the number of pos %d is %ld\n", n, tmp);
+        printf("Recursive algorithm/ the number of pos %d is %ld", n, tmp1);
 
         printf("\n");
     }
