b856bb52e7bbfb8136489a2c689d13f4cbe03782
YUWENHUI <41729370+yoyoyohui@users.noreply.github.com>
2019-11-20 23:20:03 +0800

    汉诺塔

--- 递归_汉诺塔.c
+++ 递归_汉诺塔.c
@@ -1,25 +1,36 @@
 #include<stdio.h>
-
-void hannuota(int N, char A, char B, char C){
-    if(1 == N){
-        printf("将编号为%d的盘子直接从%c柱子移动到%c柱子\n", N, A, C);
-    }
-    else{
-        hannuota(N-1, A, C, B);
-        printf("将编号为%d的盘子直接从%c柱子移动到%c柱子\n", N-1, A, C);
-        hannuota(N-1, B, A, C);
-    }
-}
+#include<math.h>
+/*move function*/
+void hannuota(int N, char A, char B, char C);
+/*caculate times*/
+void time(int M);
 
 int main(){
 
-    char ch1 = 'A';
+    char ch1 = 'A'; // three towers
     char ch2 = 'B';
     char ch3 = 'C';
-    int n;
-    printf("请输入要移动的盘子个数: ");
+    int n;  //disks number
+    printf("please input the number of disks: ");
     scanf("%d", &n);
 
-    hannuota(n, ch1, ch2, ch3);
+    hannuota(n, ch1, ch2, ch3);  //funtion
+    time(n);
+    system("pause");
     return 0;
+}
+/*move function*/
+void hannuota(int N, char A, char B, char C){
+    if(1 == N){
+        printf("put number %d disk from %c to %c\n", N, A, C);
+    }
+    else{
+        hannuota(N-1, A, C, B); //N-1个从A借助C移动到B ,N-1个就是除了最底层的剩下的
+        printf("put number %d disk from %c to %c\n", N, A, C);//N是最上面的
+        hannuota(N-1, B, A, C);
+    }
+}
+/*caculate times*/
+void time(int M){
+    printf("move %d times\n", (int)pow(2, M) - 1); //move times
 }
