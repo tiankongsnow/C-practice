fbcb1cd56d13c97f318450b18f3a9f73f49e5ec7
YUWENHUI <41729370+yoyoyohui@users.noreply.github.com>
2019-11-25 21:49:26 +0800

    递归 汉诺塔

--- 递归_汉诺塔.c
+++ 递归_汉诺塔.c
@@ -4,7 +4,7 @@
 void hannuota(int N, char A, char B, char C);
 /*caculate times*/
 void time(int M);
-
+int num = 0;
 int main(){
 
     char ch1 = 'A'; // three towers
@@ -22,12 +22,12 @@ int main(){
 /*move function*/
 void hannuota(int N, char A, char B, char C){
     if(1 == N){
-        printf("put number %d disk from %c to %c\n", N, A, C);
+        printf("%d. put number %d disk from %c to %c\n",++num, N, A, C);
     }
     else{
         hannuota(N-1, A, C, B); //N-1个从A借助C移动到B ,N-1个就是除了最底层的剩下的
-        printf("put number %d disk from %c to %c\n", N, A, C);//N是最上面的
-        hannuota(N-1, B, A, C);
+        printf("%d. put number %d disk from %c to %c\n",++num, N, A, C);//盘子序号N从上往下数1，2，3，4...
+        hannuota(N-1, B, A, C);//本来放在B,然后借助A移动到C
     }
 }
 /*caculate times*/
