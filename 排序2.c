e168c550c8ea3a1e829f116d82348e7bc974bc14
YUWENHUI <41729370+yoyoyohui@users.noreply.github.com>
2019-11-26 10:51:16 +0800

    选择排序 Selection Sort
    选择排序 Selection Sort

--- 排序2.c
+++ 排序2.c
@@ -1,37 +1,30 @@
+/*选择排序 Selection Sort */
 #include<stdio.h>
-int selection(int arr[], int n){
-    int i, j, temp;
-    int min = 9999;
-    int index;//位置下标
-    for(i=0;i<n;i++){
-        index =i;
-        min = arr[i];    //最小值
-        for(j=i; j<n; j++){
-            if(min>arr[j]){
-                index = j;    //找最小值
-                min = arr[j];
+void SelectionSort(int arr[], int size){
+    int i, j, index, tmp;
+    for(i = 0; i < size - 1; i++){
+        index = i; //纪录下标
+        for(j = i + 1; j < size; j++){
+            if(arr[j] < arr[index]){
+                index = j;
             }
         }
-        if(index != i){
-            temp = arr[i];
-            arr[i]=arr[index];
-            arr[index] = temp;//从小到大
-        }
+        tmp = arr[index];
+        arr[index] = arr[i];
+        arr[i] = tmp;
     }
-    return 0;
 }
-
 int main(){
     int i;
     int len;
-    printf("输入数组长度:");
+    printf("input the len of arr:");
     scanf("%d", &len);
-    int arr[len] ;
+    int arr[len];
     for(i = 0; i < len; i++){
         scanf("%d", &arr[i]);
     }
     //int len = sizeof(arr)/sizeof(int);     ps 计算数组长度常规方法.
-    selection(arr, len);
+    SelectionSort(arr, len);
     for(i = 0; i < len; i++){
         printf("%d ", arr[i]);
     }
